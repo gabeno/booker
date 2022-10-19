7706bb23854fcba56458f2e360d69ce7dc3e2918
Gariel Abeno <gabrielabeno@Microsoft.com>
2022-10-19 22:17:49 +0300

    variables and types:
    - assign types to variables
    - use short syntax := for variable declaration and assigning a value

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName string = "Go Conference"
+	conferenceName := "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	remainingTickets := 50
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	var userName string
+	var userTickets int
+	// ask user for their name
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
 }
